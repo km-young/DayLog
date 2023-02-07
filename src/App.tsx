e4381aa07f33f5d4e3757f05079d7b1034067b6b
hodu_Mom <aldud7798555@gmail.com>
2023-02-07 17:14:00 +0900

    feat:리코일 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from '../src/router/Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../src/styles/GlobalStyle';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 // import { theme } from 'styles/theme';
 const App = () => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       {/* <ThemeProvider> */}
+      <RecoilRoot>
         <GlobalStyle />
         <Router />
+      </RecoilRoot>
       {/* </ThemeProvider> */}
     </QueryClientProvider>
   );
